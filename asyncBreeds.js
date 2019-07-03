4d1c5e166df0dc9f72bfeeb15ba505069b15507c
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-03 03:31:14 +0000

    added files showing sync and asynchronous functions

---
+++ asyncBreeds.js
@@ -0,0 +1,16 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE 1: Pass data back via callback instead of return.
+    console.log('Callback: I have the data!')
+    if (!error) callback(data); 
+  });
+}
+
+// CHANGE 2: Value now comes back via callback, not return value
+breedDetailsFromFile('Bombay', (bombay) => {
+  console.log('Return Value: ', bombay) // => print out details correctly.
+});
